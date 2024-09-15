68acfb2f6901b2fd09c0a772ef058f31564984e7
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-09-15 23:47:33 +0900

    Comment : 모범답안 풀이 추가

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,3 +1,4 @@
+/** 내 답안 **/
 class Solution {
     public int solution(int n) {
         int result = 0;
@@ -8,5 +9,19 @@ class Solution {
             num += 2;
         }
         return result;
+    }
+}
+
+
+/** 모범 답안 **/
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=2; i<=n; i+=2){
+            answer+=i;
+        }
+
+        return answer;
     }
 }
