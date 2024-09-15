0c72a02fd95f1e3777aa7fc71db17a795de7cc1f
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-09-15 21:10:11 +0900

    Comment : 모범답안 & 설명 추가

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,3 +1,4 @@
+/** 내 답안 **/
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
@@ -7,6 +8,16 @@ class Solution {
         else {
             answer = -1;
         }
+        return answer;
+    }
+}
+
+
+/** 모범 답안 **/
+/** if문 보다는 삼항연산자로 간단하게 해결할 수 있음 **/
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = (num1 == num2) ? 1 : -1;
         return answer;
     }
 }
