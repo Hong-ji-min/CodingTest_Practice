d77ad0b041ac45d9358722438d214db04fb10c0c
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-09-24 15:54:46 +0900

    Comment :  옷가게 할인 받기 정답률 85% 코드 추가

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -12,3 +12,27 @@ class Solution {
         return price;
     }
 }
+
+/** 정답률이 85% 인 답 **/
+/** 🚨반례를 찾아보기🚨 **/
+import java.util.*;
+
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(price >= 500000) {
+            answer = (int) (price * 0.8);
+        }
+        else if (price >= 300000) {
+            answer = (int) (price * 0.9);
+        }
+        else if (price >= 10000) {
+            answer = (int) (price * 0.95);   
+        }
+        else {
+            answer = price;
+
+        }
+        return answer;
+    }
+}
