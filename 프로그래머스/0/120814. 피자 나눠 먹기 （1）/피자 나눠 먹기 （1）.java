a78615586f91c53e2d886d7b302562c8040ca81c
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-09-24 15:13:28 +0900

    Comment : 피자 나눠 먹기 모범 답안 추가

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,3 +1,4 @@
+/** 내 답안 **/
 class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -9,5 +10,13 @@ class Solution {
             answer = n / 7 + 1;
         }
         return answer;
+    }
+}
+
+/** 모범 답안 **/
+/** 나머지가 0 ~6만 나온다는 성질 이용 **/
+class Solution {
+    public int solution(int n) {
+        return (n + 6) / 7;
     }
 }
