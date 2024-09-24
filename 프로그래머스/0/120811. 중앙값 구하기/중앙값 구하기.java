cc002234ff2cb30d0fb1c2ceb415da89bc1b05bb
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-09-24 14:18:19 +0900

    Comment : 중앙값 구하기 설명 추가

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -3,7 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        
+
+        /** 배열은 index가 0부터 시작한다는 것을 항상 명심할 것! **/
         int answer = array[array.length/2];
         return answer;
     }
