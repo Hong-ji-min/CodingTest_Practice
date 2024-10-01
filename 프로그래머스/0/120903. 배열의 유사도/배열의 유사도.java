a6f3c1a61d172ac8ff546b0947adee54c57e96fb
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-10-01 18:07:34 +0900

    Comment : 배열의 유사도 고민해보기

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -4,6 +4,8 @@ class Solution {
         
         for(int i = 0; i < s1.length; i++) {
             for (int j = 0; j < s2.length; j++) {
+                
+                // == 이랑 차이점 생각해보기
                 if(s1[i].equals(s2[j])) {
                     answer++;
                 }
