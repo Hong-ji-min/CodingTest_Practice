526c418f352604cd94daa2a5bde5c24946da46ef
Hojiiming <80135858+Hong-ji-min@users.noreply.github.com>
2024-10-01 18:57:59 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.18 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] answer = my_string.split("");
+        
+        
+        String str = answer[num1];
+        answer[num1] = answer[num2];
+        answer[num2] = str;
+        
+        String result = String.join("", answer);
+        return result;
+    }
+}
